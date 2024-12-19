84ce6f23ffcd5d5e77fa2174d8f36a73b9a376c5
edusalam <eduarfernando.salcedo@gmail.com>
2024-12-19 00:18:29 -0500

    Update main.py

---
+++ Modulo2_clase1/main.py
@@ -0,0 +1,7 @@
+#definimos una clase
+#metodo constructor para python
+def __init__(self, atributos1, atributo2):
+    self.atributo1 = valor1
+    self.atributo2 = valor2
+def comportamientoFuncionDefinicion(self):
+    pass
