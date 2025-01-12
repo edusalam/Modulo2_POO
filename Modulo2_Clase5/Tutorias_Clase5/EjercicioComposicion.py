56471b0a2a92e97193e1c4d3f665a5d9bc8ebeac
edusalam <eduarfernando.salcedo@gmail.com>
2025-01-12 01:39:40 -0500

    EjercicioComposicion1 tutoria 5

---
+++ Modulo2_Clase5/Tutorias_Clase5/EjercicioComposicion.py
@@ -0,0 +1,29 @@
+# lo primero es definir una clase
+class Motor:
+    def __init__(self, tipo):
+        self.tipo = tipo
+    
+    def encender(self):
+        print(f'el motoor de tipo {self.tipo} se ha encendido')
+class Rueda:
+    def __init__(self, tamano):
+        self.tamano = tamano
+
+    def girar(self):
+        print(f'la rueda de tamaño {self.tamano} esta girando')
+
+class Coche:
+    def __init__(self, motor, ruedas):
+        self.motor = motor
+        self.ruedas = ruedas
+
+    def iniciar_viaje(self):
+        self.motor.encender()
+        for rueda in self.ruedas:
+            rueda.girar()
+        print('el coche ha comenzado a moverse')
+motor_deportivo = Motor('v10')
+ruedas_pequeñas = [Rueda(14),Rueda(14),Rueda(14),Rueda(14),]
+
+coche_deportivo = Coche(motor_deportivo, ruedas_pequeñas)
+coche_deportivo.iniciar_viaje()
