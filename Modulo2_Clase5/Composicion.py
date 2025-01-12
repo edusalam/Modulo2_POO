064cb7823a18fa5fc99f4c199af70f233940f75a
edusalam <eduarfernando.salcedo@gmail.com>
2025-01-11 22:24:49 -0500

    EJERCICIO COMPOSISION CLASE 5

--- Modulo2_Clase5/Composicion.py
+++ Modulo2_Clase5/Composicion.py
@@ -1,1 +1,37 @@
 #EJERCICION DE COMPOSICION
+class Vehiculo:
+    def __init__(self, motor):
+        self.motor = motor   #ESTO SE LLAMA ENCAPSULAMIENTO
+        
+    def encender(self):
+        print('encendiendo el vehiculo')
+        self.motor.iniciar()
+
+
+class Motor:
+    def __init__(self):
+        pass
+    def iniciar(self):
+        pass
+class MotorGasolina(Motor):
+    def __init__(self):
+        pass
+    def iniciar(self):
+        print('motor a gasolina encendido')
+
+class MotorElectrico(Motor):
+    def __init__(self):
+        pass
+    def iniciar(self):
+        print('motor electrico encendido')
+
+#CREAMOS LOS OBJETOS        
+motor_gasolina = MotorGasolina()
+motor_electrico = MotorElectrico()
+
+# COMO PARTE DE LA COMPOSICION LO ESTOY ATANDO AL VEHICULO CON LOS TIPOS DE MOTOROS
+auto_gasolina = Vehiculo(motor_gasolina)
+auto_electrico = Vehiculo(motor_electrico)
+
+auto_gasolina.encender()
+auto_electrico.encender()
